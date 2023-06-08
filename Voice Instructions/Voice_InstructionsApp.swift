edd30f5ff96103dc1e25aafe0f3e19c7dd35239e
Taananas <t.rybalkina@brightlab.me>
2023-06-08 10:49:05 +0300

    Initial Commit

---
+++ Voice Instructions/Voice_InstructionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_InstructionsApp.swift
+//  Voice Instructions
+//
+//  Created by Bogdan Zykov on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Voice_InstructionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
